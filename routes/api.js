0bb8984d2a0833e6a6afc101e38ab875fefbf4d9
Chay Williams <Chayvw@gmail.com>
2020-07-29 19:53:48 -0400

    api royte added a range route so front end could use it and corrected a typo in my create route and in my html added a route to render the index

--- routes/api.js
+++ routes/api.js
@@ -3,17 +3,8 @@ const Workout = require("../models/workout")
 
 const db = require("../models");
 
-router.get("/api/workout", (req, res) => {
-   Workout.find({})
 
-      .then(dbWorkout => {
-        res.json(dbWorkout);
-      })
-      .catch(err => {
-        res.status(400).json(err);
-      });
-  });
-
+// Route to create a workout 
   router.post("/api/workouts", ({ body }, res) => {
     db.Workout.create(body)
       .then((dbWorkout) => {
@@ -23,10 +14,11 @@ router.get("/api/workout", (req, res) => {
         res.json(err);
       });
   });
-
+// route to update a workout
   router.put("/api/workouts/:id", (req, res) => {
     db.Workout.findOneAndUpdate(
-      { _id: req.params.id },
+        req.params.find,
+   
       { $push: { exercises: req.body } }
     )
       .then((dbWorkout) => {
@@ -34,6 +26,31 @@ router.get("/api/workout", (req, res) => {
       })
       .catch((err) => {
         res.json(err);
+      });
+  });
+
+  // route set up to get the last workouts
+  router.get("/api/workouts", (req, res) => {
+    Workout.find({})
+ 
+       .then(dbWorkout => {
+         res.json(dbWorkout);
+       })
+       .catch(err => {
+         res.status(400).json(err);
+       });
+   });
+
+   // Using class mini project example for refrence was able to set a route to
+    // fetch the data for the front end to make the charts
+   router.get("/api/workouts/range", (req, res) => {
+    db.Workout.find({})
+      .limit(7) 
+      .then((dbWorkout) => {
+        res.json(dbWorkout);
+      })
+      .catch((err) => {
+        res.status(500).json(err);
       });
   });
   
